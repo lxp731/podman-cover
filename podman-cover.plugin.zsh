c2d12ca4465dcd46e9fb984ccf7cfa92aca460b2
Burgess Leo <liuxiaopeng731@gmail.com>
2025-09-28 15:09:19 +0800

    print full_repo

--- podman-cover.plugin.zsh
+++ podman-cover.plugin.zsh
@@ -2,7 +2,6 @@ podman() {
   if [[ "$1" == "images" ]]; then
     shift
 
-    # 使用 format 输出四列：Repository, Tag, ID, Size
     command podman images --format "{{.Repository}}\t{{.Tag}}\t{{.ID}}\t{{.Size}}" | \
     awk '
     function fix_size(s) {
@@ -18,41 +17,41 @@ podman() {
 
     BEGIN {
       FS = "\t"
-      # 初始化最大宽度
       max_repo = length("REPOSITORY")
       max_tag  = length("TAG")
       max_id   = length("IMAGE ID")
       max_size = length("SIZE")
+      max_full = length("FULL_REPO")
     }
 
     {
-      # 简化仓库名
-      repo = $1
-      gsub(/^docker\.io\/library\//, "", repo)
-      gsub(/^docker\.io\//, "", repo)
+      full_repo = $1
 
-      # 更新最大宽度
-      if (length(repo)    > max_repo) max_repo = length(repo)
-      if (length($2)      > max_tag)  max_tag  = length($2)
-      if (length($3)      > max_id)   max_id   = length($3)
-      if (length(fix_size($4)) > max_size) max_size = length(fix_size($4))
+      # 简化仓库名：始终只取最后一段
+      n = split($1, parts, "/")
+      repo = parts[n]
 
-      # 存储数据
-      repolist[NR] = repo
-      taglist[NR]  = $2
-      idlist[NR]   = $3
-      sizelist[NR] = fix_size($4)
+      if (length(repo)        > max_repo) max_repo = length(repo)
+      if (length($2)          > max_tag)  max_tag  = length($2)
+      if (length($3)          > max_id)   max_id   = length($3)
+      if (length(fix_size($4))> max_size) max_size = length(fix_size($4))
+      if (length(full_repo)   > max_full) max_full = length(full_repo)
+
+      repolist[NR]  = repo
+      taglist[NR]   = $2
+      idlist[NR]    = $3
+      sizelist[NR]  = fix_size($4)
+      fulllist[NR]  = full_repo
     }
 
     END {
-      # 打印表头（左对齐）
-      printf "%-*s  %-*s  %-*s  %-*s\n",
+      printf "%-*s  %-*s  %-*s  %-*s  %-*s\n",
         max_repo, "REPOSITORY",
         max_tag,  "TAG",
         max_id,   "IMAGE ID",
-        max_size, "SIZE"
+        max_size, "SIZE",
+        max_full, "FULL_REPO"
 
-      # 构造分隔线（严格对齐）
       line = ""
       for (i = 1; i <= max_repo; i++) line = line "-"
       line = line "  "
@@ -61,19 +60,20 @@ podman() {
       for (i = 1; i <= max_id; i++) line = line "-"
       line = line "  "
       for (i = 1; i <= max_size; i++) line = line "-"
+      line = line "  "
+      for (i = 1; i <= max_full; i++) line = line "-"
       print line
 
-      # 打印数据行
       for (i = 1; i <= NR; i++) {
-        printf "%-*s  %-*s  %-*s  %-*s\n",
+        printf "%-*s  %-*s  %-*s  %-*s  %-*s\n",
           max_repo, repolist[i],
           max_tag,  taglist[i],
           max_id,   idlist[i],
-          max_size, sizelist[i]
+          max_size, sizelist[i],
+          max_full, fulllist[i]
       }
     }'
   else
     command podman "$@"
   fi
 }
-
